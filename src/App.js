6e05bf06a9f48b736ef0f737ace25a54595cca5e
Syed-Muhammad-Owais <owais_bila@hotmail.com>
2021-12-29 16:45:07 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './Header';
+import Footer from './Footer';
+import Dashboard from './Dashboard';
+import Menu from './Menu';
+
+function App() {
+  return (
+    <div className="wraper">
+<Header/>
+<Footer/>
+<Dashboard/>
+<Menu/>
+
+    </div>
+  );
+}
+
+export default App;
